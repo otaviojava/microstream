231e289ca7938c6cbbe1198c877838a3eab8b09c
hg-ms <53219833+hg-ms@users.noreply.github.com>
2022-02-14 14:05:39 +0100

    logging

--- communication/binary/src/main/java/one/microstream/communication/binarydynamic/ComTypeMappingResolver.java
+++ communication/binary/src/main/java/one/microstream/communication/binarydynamic/ComTypeMappingResolver.java
@@ -2,6 +2,8 @@ package one.microstream.communication.binarydynamic;
 
 import java.nio.ByteBuffer;
 
+import org.slf4j.Logger;
+
 import one.microstream.chars.VarString;
 import one.microstream.chars.XChars;
 import one.microstream.collections.BulkList;
@@ -15,19 +17,22 @@ import one.microstream.persistence.types.PersistenceTypeDictionaryAssembler;
 import one.microstream.persistence.types.PersistenceTypeDictionaryView;
 import one.microstream.persistence.types.PersistenceTypeHandler;
 import one.microstream.persistence.types.PersistenceTypeHandlerManager;
+import one.microstream.util.logging.Logging;
 
 /**
  * This class handles the matching of types that have been modified on either the client or the host side
  * Including the necessary data transfer during the initialization of the ComChannels.
  *
  */
-public class ComTypeMappingResolver 
-{	
+public class ComTypeMappingResolver
+{
 	///////////////////////////////////////////////////////////////////////////
 	// constants //
 	//////////////
 	
 	protected static final int LENGTH_CHAR_COUNT = 8;
+		
+	private final static Logger logger = Logging.getLogger(ComHandlerSendMessageNewType.class);
 	
 	
 	///////////////////////////////////////////////////////////////////////////
@@ -56,11 +61,11 @@ public class ComTypeMappingResolver
 	 */
 	public ComTypeMappingResolver(
 		final PersistenceTypeDictionaryAssembler    typeDictionaryAssembler,
-		final ComConnection                         connection, 
+		final ComConnection                         connection,
 		final PersistenceTypeDictionaryView         hostTypeDictionary,
 		final PersistenceTypeHandlerManager<Binary> typeHandlerManager,
 		final ComTypeDefinitionBuilder              typeDefinitionBuilder
-	) 
+	)
 	{
 		super();
 		this.typeDictionaryAssembler = typeDictionaryAssembler;
@@ -78,10 +83,12 @@ public class ComTypeMappingResolver
 	/**
 	 * Handle the client's side of the communication type mapping during connection initialization phase.
 	 * This is collection all type definition that belong to the clients classes that needs to be mapped by the host
-	 * and transferring those to the host. 
+	 * and transferring those to the host.
 	 */
 	public void resolveClient()
-	{						
+	{
+		logger.debug("resolving client type mappings ");
+		
 		this.sendNewTypeDefintionsToHost(
 			this.assembleTypeDefinitions(
 				this.findHostTypeDefinitions()));
@@ -89,27 +96,31 @@ public class ComTypeMappingResolver
 	
 	/**
 	 * Handle the host's side of the communication type mapping during connection initialization phase.
-	 * This is receiving the client's type definitions and creating the required legacy type handlers.  
+	 * This is receiving the client's type definitions and creating the required legacy type handlers.
 	 */
-	public void resolveHost() 
+	public void resolveHost()
 	{
+		logger.debug("resolving host type mappings ");
+		
 		this.applyHostTypeMapping(
 			this.parseClientTypeDefinitions(
 				this.receiveUpdatedDefintionsfromClient()));
 	}
 	
-	private void sendNewTypeDefintionsToHost(final byte[] assembledTypeDefinitions) 
+	private void sendNewTypeDefintionsToHost(final byte[] assembledTypeDefinitions)
 	{
+		logger.trace("transfering new type defintions to host");
+		
 		final ByteBuffer dbb = XMemory.allocateDirectNative(assembledTypeDefinitions.length);
 		final long dbbAddress = XMemory.getDirectByteBufferAddress(dbb);
 		XMemory.copyArrayToAddress(assembledTypeDefinitions, dbbAddress);
 		
-		this.connection.writeCompletely(dbb);	
+		this.connection.writeCompletely(dbb);
 	}
 
-	private byte[] assembleTypeDefinitions(final BulkList<PersistenceTypeDescription> newDefinitions) 
+	private byte[] assembleTypeDefinitions(final BulkList<PersistenceTypeDescription> newDefinitions)
 	{
-		final VarString vs = VarString.New(10_000);		
+		final VarString vs = VarString.New(10_000);
 		
 		vs
 		.reset()
@@ -126,7 +137,7 @@ public class ComTypeMappingResolver
 		return vs.encode();
 	}
 
-	private VarString assembleTypeDefintion(final PersistenceTypeDescription definition) 
+	private VarString assembleTypeDefintion(final PersistenceTypeDescription definition)
 	{
 		final VarString vc = VarString.New();
 		this.typeDictionaryAssembler.assembleTypeDescription(vc, definition);
@@ -137,33 +148,41 @@ public class ComTypeMappingResolver
 	{
 		final BulkList<PersistenceTypeDescription> newTypeDescriptions = BulkList.New();
 		
-		this.typeHandlerManager.iterateLegacyTypeHandlers(legacyTypeHandler -> {					
+		this.typeHandlerManager.iterateLegacyTypeHandlers(legacyTypeHandler -> {
 			final PersistenceTypeHandler<Binary, ?> currentHandler = this.typeHandlerManager.lookupTypeHandler(legacyTypeHandler.type());
 			if(this.hostTypeDictionary.lookupTypeById(currentHandler.typeId()) == null)
 			{
 				newTypeDescriptions.add(currentHandler);
+				logger.trace("new type found for id {}", currentHandler.typeId());
 			}
 		});
 		
+		logger.debug("{} new types found", newTypeDescriptions.size());
 		return newTypeDescriptions;
 	}
 	
-	private XGettingSequence<PersistenceTypeDefinition> parseClientTypeDefinitions(final ByteBuffer buffer) 
+	private XGettingSequence<PersistenceTypeDefinition> parseClientTypeDefinitions(final ByteBuffer buffer)
 	{
-		if(buffer != null) 
+		if(buffer != null)
 		{
 			buffer.position(1);
 			final char[] typeDefinitionsChars = XChars.standardCharset().decode(buffer).array();
 		
 			final String typeDefintions = XChars.String(typeDefinitionsChars);
-			return this.typeDefinitionBuilder.buildTypeDefinitions(typeDefintions);
+			final XGettingSequence<PersistenceTypeDefinition> newTypeDescriptions = this.typeDefinitionBuilder.buildTypeDefinitions(typeDefintions);
+			
+			logger.debug("received {} types from client", newTypeDescriptions.size());
+			return newTypeDescriptions;
 		}
 		
+		logger.debug("received 0 types from client");
 		return BulkList.New();
 	}
 
-	private ByteBuffer receiveUpdatedDefintionsfromClient() 
+	private ByteBuffer receiveUpdatedDefintionsfromClient()
 	{
+		logger.trace("receiving new type defintions from client");
+		
 		final ByteBuffer lengthBuffer = XMemory.allocateDirectNative(LENGTH_CHAR_COUNT);
 		this.connection.read(lengthBuffer, LENGTH_CHAR_COUNT);
 		
@@ -177,10 +196,10 @@ public class ComTypeMappingResolver
 		final String countDigits = XChars.standardCharset().decode(countBuffer).toString();
 		final int    count       = Integer.parseInt(countDigits);
 		
-		if(count > 0 ) 
+		if(count > 0 )
 		{
 			final ByteBuffer typeDefinitionsBuffer = XMemory.allocateDirectNative(length - LENGTH_CHAR_COUNT - LENGTH_CHAR_COUNT);
-			this.connection.read(typeDefinitionsBuffer, length - LENGTH_CHAR_COUNT - LENGTH_CHAR_COUNT);			
+			this.connection.read(typeDefinitionsBuffer, length - LENGTH_CHAR_COUNT - LENGTH_CHAR_COUNT);
 			return typeDefinitionsBuffer;
 		}
 		
@@ -191,10 +210,12 @@ public class ComTypeMappingResolver
 	{
 		if(typeDefinitions != null)
 		{
-			typeDefinitions.forEach( typeDefinition -> {		
-				final PersistenceTypeHandler<Binary, ?> currentHandler = this.typeHandlerManager.lookupTypeHandler(typeDefinition.type());	
-				this.typeHandlerManager.ensureLegacyTypeHandler(typeDefinition, currentHandler);	
+			typeDefinitions.forEach( typeDefinition -> {
+				final PersistenceTypeHandler<Binary, ?> currentHandler = this.typeHandlerManager.lookupTypeHandler(typeDefinition.type());
+				this.typeHandlerManager.ensureLegacyTypeHandler(typeDefinition, currentHandler);
 				this.typeHandlerManager.updateCurrentHighestTypeId(typeDefinition.typeId());
+				
+				logger.trace("type mapping applied for typeId {}", typeDefinition.typeId());
 			});
 		}
 	}
